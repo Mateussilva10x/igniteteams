26009aae0b050bdd10e183f1f4374b8e965a8979
Mateus Silva <mateussilva651@gmail.com>
2023-09-19 09:11:44 -0300

    feat: use icons to the header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,15 @@
-import { Container, Logo } from "./HeaderStyles";
+import { BackButton, BackIcon, Container, Logo } from "./HeaderStyles";
 
 import logoImg from "../../assets/logo.png";
 
-export function Header() {
+export function Header({ showBackButton = false }) {
   return (
     <Container>
+      {showBackButton && (
+        <BackButton>
+          <BackIcon />
+        </BackButton>
+      )}
       <Logo source={logoImg} />
     </Container>
   );
