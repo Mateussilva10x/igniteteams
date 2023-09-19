af592bb85f16469f573298c6e8520db69477ba43
Mateus Silva <mateussilva651@gmail.com>
2023-09-19 08:40:29 -0300

    feat: add fonts and create loading

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import { ThemeProvider } from "styled-components/native";
 import Groups from "./src/screens/Groups";
 import theme from "./src/theme";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { Loading } from "./src/components/loading";
+import { StatusBar } from "react-native";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      <StatusBar
+        barStyle="light-content"
+        translucent
+        backgroundColor="transparent"
+      />
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
