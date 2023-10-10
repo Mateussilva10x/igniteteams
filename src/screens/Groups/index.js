53fbc5329b9bdc5ae6d5530105a5edf3fb2dc8d4
Mateus Silva <mateussilva651@gmail.com>
2023-10-09 21:35:47 -0300

    feat(groups): use the groupCard in a flat list

--- src/screens/Groups/index.js
+++ src/screens/Groups/index.js
@@ -1,14 +1,23 @@
+import { useState } from "react";
 import { GroupCard } from "../../components/GroupCard";
 import { Header } from "../../components/Header";
 import { Highlight } from "../../components/Highlight";
 import { Container } from "./styles";
+import { FlatList } from "react-native";
 
 export default function Groups() {
+  const [groups, setGroups] = useState([]);
+
   return (
     <Container>
-      <Header showBackButton />
+      <Header />
       <Highlight title="Turmas" subtitle="Jogue com sua turma" />
-      <GroupCard title="Galera do ignite" />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+      />
     </Container>
   );
 }
